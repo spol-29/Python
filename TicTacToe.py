cd54aeaca5d5b2b696a81c96525b2c3423a3b44d
spol-29 <polineni.sriram9@gmail.com>
2023-04-13 22:42:34 -0500

    tictactoe export

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,4 +1,5 @@
 import random
+print("This game is about stealing the opponents spot on the grid and winning the game!")
 
 class TicTacToe:
 
@@ -11,18 +12,13 @@ class TicTacToe:
             for j in range(3):
                 row.append('-')
             self.board.append(row) 
-
     def get_random_first_player(self):
         return random.randint(0, 1)
-
     def fix_spot(self, row, col, player):
         self.board[row][col] = player
-
     def is_player_win(self, player):
         win = None
-
         n = len(self.board)
-
         for i in range(n):
             win = True 
             for j in range(n):
@@ -31,7 +27,6 @@ class TicTacToe:
                     break
             if win:
                 return win
-
         for i in range(n):
             win = True
             for j in range(n):
@@ -40,7 +35,6 @@ class TicTacToe:
                     break
             if win:
                 return win
-
         win = True
         for i in range(n):
             if self.board[i][i] != player:
@@ -48,7 +42,6 @@ class TicTacToe:
                 break
         if win:
             return win
-
         win = True
         for i in range(n):
             if self.board[i][n - 1 - i] != player:
@@ -57,13 +50,11 @@ class TicTacToe:
         if win:
             return win
         return False
-
         for row in self.board:
             for item in row:
                 if item == '-':
                     return False
         return True
-
     def is_board_filled(self):
         for row in self.board:
             for item in row:
@@ -79,36 +70,25 @@ class TicTacToe:
             for item in row:
                 print(item, end=" ")
             print()
-
     def start(self):
         self.create_board()
-
         player = 'X' if self.get_random_first_player() == 1 else 'O'
         while True:
             print(f"Player {player} turn")
-
             self.show_board()
-            print("This game is about stealing the opponents spot on the grid and winning the game!")
             print("Enter the ROW followed by a space then input the COLUMN number")
             row, col = list(map(int, input("Enter row and column numbers to fix spot(Y X): ").split()))
-                
             print()
-
             self.fix_spot(row - 1, col - 1, player)
-
             if self.is_player_win(player):
                 print(f"Player {player} wins the game!")
                 break
-
             if self.is_board_filled():
                 print("Match Draw!")
                 break
-
             player = self.swap_player_turn(player)
-
         print()
         self.show_board()
-
 
 tic_tac_toe = TicTacToe()
 tic_tac_toe.start()
