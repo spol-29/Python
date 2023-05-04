4d75e4c4296becfc7ef64933322164b44e16ba7a
spol-29 <polineni.sriram9@gmail.com>
2023-05-03 19:26:06 -0500

    python projects

--- Calculator.py
+++ Calculator.py
@@ -29,7 +29,6 @@ while True:
 
         elif choice == '2':
             print(num1, "-", num2, "=", subtract(num1, num2))
-
         elif choice == '3':
             print(num1, "*", num2, "=", multiply(num1, num2))
         elif choice == '4':
